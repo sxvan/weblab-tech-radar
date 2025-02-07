8391f9eb08879056d8bd350c1cf10777e85ae9b7
Silvan Büchi <silvan.buechi01@outlook.com>
2025-02-07 14:57:34 +0100

    feat(technology): added update, delete and publish mutations for technology

--- api/src/resolvers/technology.resolver.ts
+++ api/src/resolvers/technology.resolver.ts
@@ -1,9 +1,9 @@
 import 'reflect-metadata';
-import { Arg, Ctx, Mutation, Query } from 'type-graphql';
+import { Arg, Ctx, ID, Mutation, Query } from 'type-graphql';
 import { Technology } from '../types/technology.type';
 import { ApolloServerContext } from '../contexts/apollo-server.context';
-import { Prisma } from '@prisma/client';
 import { CreateTechnologyInput } from '../inputs/create-technology.input';
+import { UpdateTechnologyInput } from '../inputs/update-technology.input';
 
 export class TechnologyResolver {
     @Query(() => [Technology])
@@ -22,10 +22,46 @@ export class TechnologyResolver {
 
     @Mutation(() => Technology)
     async createTechnology(@Arg('data', () => CreateTechnologyInput) data: CreateTechnologyInput, @Ctx() ctx: ApolloServerContext) {
-        const technology: Prisma.TechnologyCreateInput = { ...data, createdAt: new Date(), changedAt: new Date() };
-
         return ctx.prisma.technology.create({
-            data: technology,
+            data: {
+                ...data,
+                createdAt: new Date(),
+                changedAt: new Date(),
+            },
+        });
+    }
+
+    @Mutation(() => Technology)
+    async updateTechnology(@Arg('id', () => ID) id: string, @Arg('data', () => UpdateTechnologyInput) data: UpdateTechnologyInput, @Ctx() ctx: ApolloServerContext) {
+        return ctx.prisma.technology.update({
+            where: {
+                id: id,
+            },
+            data: {
+                ...data,
+                changedAt: new Date(),
+            },
+        });
+    }
+
+    @Mutation(() => Technology)
+    async deleteTechnology(@Arg('id', () => ID) id: string, @Ctx() ctx: ApolloServerContext) {
+        return ctx.prisma.technology.delete({
+            where: {
+                id: id,
+            },
+        });
+    }
+
+    @Mutation(() => Technology)
+    async publishTechnology(@Arg('id', () => ID) id: string, @Ctx() ctx: ApolloServerContext) {
+        return ctx.prisma.technology.update({
+            where: {
+                id: id,
+            },
+            data: {
+                publishedAt: new Date(),
+            },
         });
     }
 }
